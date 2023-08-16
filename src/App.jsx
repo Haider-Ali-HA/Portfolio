428525f9dbb9300b5d035f30bb2b7ddfe9628e31
Haider Ali <haideraliatish500@gmail.com>
2023-08-16 21:37:34 +0500

    portfolio project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './component/Navbar'
+import Intro from './component/Intro'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Intro />
+    </>
+  )
+}
+
+export default App
