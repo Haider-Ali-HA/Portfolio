bdcc32a7ddfcf5157f0c6c82c52d2d54eb52bc9d
Haider Ali <haideraliatish500@gmail.com>
2023-08-23 15:18:41 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,13 @@ import Clients from "./component/Clients";
 import Contact from "./component/Contact";
 import SocialIcon from "./component/SocialIcon";
 import Footer from "./component/Footer";
-import { useState, useEffect } from "react";
-import Hashloader from "react-spinners/Hashloader";
 
 function App() {
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 5000);
-  }, []);
+ 
 
   return (
     < >
-      {loading ? (
-        <div className="flex justify-center h-full w-full items-center mt-[70%] sm:mt-[40%] md:mt-[30%] lg:mt-[20%]">
-
-        <Hashloader
-         
-         color={"#e30b2c"}
-         loading={loading}
-         size={50}
-         aria-label="Loading Spinner"
-         data-testid="loader"
-         />
-         </div>
-      ) : (
-        <div>
+      
           <Navbar />
           <Intro />
           <Skilled />
@@ -43,8 +22,7 @@ function App() {
           <Contact />
           <SocialIcon />
           <Footer />
-        </div>
-      )}
+        
     </>
   );
 }
